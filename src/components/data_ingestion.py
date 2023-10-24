16723b3d9ea2394a76e1761e7c9e0bb4ab963b24
Mayank Borasi <rm.mayankb@gmail.com>
2023-10-24 05:32:24 +0530

    Data Transformation Done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,12 +1,13 @@
 import os
 import sys
-from src.exception import CustomException
-from src.logger import logging
 import pandas as pd
-
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.exception import CustomException
+from src.logger import logging
+from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
 
 @dataclass # (Directly define the class variables without __init__)
 class DataIngestionConfig:
@@ -22,7 +23,6 @@ class DataIngestion:
     def __init__(self):
         self.ingestion_config = DataIngestionConfig()
 
-    # 
     def initiate_data_ingestion(self):
         logging.info('Entered The Data Ingestion Method or Component')
 
@@ -54,4 +54,6 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
